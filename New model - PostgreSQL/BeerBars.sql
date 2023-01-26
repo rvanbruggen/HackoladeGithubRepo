a01c7d0e9a66686376d5ad2a4b7d056abfef51bb
Rik Van Bruggen <rik@neotechnology.com>
2023-01-26 18:21:11 +0100

    Updates in various places

---
+++ New model - PostgreSQL/BeerBars.sql
@@ -0,0 +1,16 @@
+CREATE SCHEMA BeerBars;
+SET search_path TO BeerBars;
+
+CREATE TABLE BeerBars.Bar (
+	name varchar(10),
+	capacity smallint
+);
+
+CREATE TABLE BeerBars.Address (
+	Streetname varchar(10),
+	Streetnumber smallint,
+	Zipcode varchar(10),
+	City varchar(10),
+	Country varchar(10),
+	Location composite
+);
