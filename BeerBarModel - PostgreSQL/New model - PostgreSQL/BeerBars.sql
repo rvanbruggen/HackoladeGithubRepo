86fcb8d8ee13d12091e3e782b80ce97f806206c4
Rik Van Bruggen <rik@neotechnology.com>
2023-01-27 15:29:28 +0100

    Beergraph v1

--- BeerBarModel - PostgreSQL/New model - PostgreSQL/BeerBars.sql
+++ BeerBarModel - PostgreSQL/New model - PostgreSQL/BeerBars.sql
@@ -2,14 +2,14 @@ CREATE SCHEMA BeerBars;
 SET search_path TO BeerBars;
 
 CREATE TABLE BeerBars.Bar (
-	name varchar(10),
+	name varchar(100),
 	capacity smallint
 );
 
 CREATE TABLE BeerBars.Address (
-	Streetname varchar(10),
+	Streetname varchar(100),
 	Streetnumber smallint,
 	Zipcode varchar(10),
-	City varchar(10),
-	Country varchar(10)
+	City varchar(50),
+	Country varchar(100)
 );
