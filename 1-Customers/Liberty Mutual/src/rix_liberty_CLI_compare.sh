2d0e9fb7affd11db32aa26aee9359bdbac7fff1a
Rik Van Bruggen <rik@neotechnology.com>
2023-06-22 08:26:10 +0200

    New version with a lot of updates!
    New version!

--- 1-Customers/Liberty Mutual/src/rix_liberty_CLI_compare.sh
+++ 1-Customers/Liberty Mutual/src/rix_liberty_CLI_compare.sh
@@ -18,20 +18,20 @@ swagger-cli bundle ./v1/master.yaml -o ./swaggercli/rix_swaggercli_liberty_opena
 swagger-cli bundle ./v2/master.yaml -o ./swaggercli/rix_swaggercli_liberty_openapi_v2_dereferenced.yaml -r;
 
 # Reverse engineer the combined SWAGGERCLI YAML files into a model
-/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="./swaggercli/rix_swaggercli_liberty_openapi_v1.yaml" --model=/Users/rvanbruggen/Downloads/src/swaggercli/rix_swaggercli_liberty_openapi_v1_CLI.hck.json;
+/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="./swaggercli/rix_swaggercli_liberty_openapi_v1.yaml" --model=./swaggercli/rix_swaggercli_liberty_openapi_v1_CLI.hck.json;
 
-/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="./swaggercli/rix_swaggercli_liberty_openapi_v2.yaml" --model=/Users/rvanbruggen/Downloads/src/swaggercli/rix_swaggercli_liberty_openapi_v2_CLI.hck.json;
+/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="./swaggercli/rix_swaggercli_liberty_openapi_v2.yaml" --model=./swaggercli/rix_swaggercli_liberty_openapi_v2_CLI.hck.json;
 
 # Reverse engineer the combined DEREFERENCED SWAGGERCLI YAML files into a model
-/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="./swaggercli/rix_swaggercli_liberty_openapi_v1_dereferenced.yaml" --model=/Users/rvanbruggen/Downloads/src/swaggercli/rix_swaggercli_liberty_openapi_dereferenced_v1_CLI.hck.json;
+/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="./swaggercli/rix_swaggercli_liberty_openapi_v1_dereferenced.yaml" --model=./swaggercli/rix_swaggercli_liberty_openapi_dereferenced_v1_CLI.hck.json;
 
-/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="./swaggercli/rix_swaggercli_liberty_openapi_v2_dereferenced.yaml" --model=/Users/rvanbruggen/Downloads/src/swaggercli/rix_swaggercli_liberty_openapi_dereferenced_v2_CLI.hck.json;
+/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="./swaggercli/rix_swaggercli_liberty_openapi_v2_dereferenced.yaml" --model=./swaggercli/rix_swaggercli_liberty_openapi_dereferenced_v2_CLI.hck.json;
 
 # Comparing two SWAGGERCLI models
-/Applications/Hackolade.app/Contents/MacOS/Hackolade compMod --model1=/Users/rvanbruggen/Downloads/src/swaggercli/rix_swaggercli_liberty_openapi_v1_CLI.hck.json --model2=/Users/rvanbruggen/Downloads/src/swaggercli/rix_swaggercli_liberty_openapi_v2_CLI.hck.json --deltamodel=./swaggercli/rix_swaggercli_liberty_openapi_deltamodel.json;
+/Applications/Hackolade.app/Contents/MacOS/Hackolade compMod --model1=./src/swaggercli/rix_swaggercli_liberty_openapi_v1_CLI.hck.json --model2=./swaggercli/rix_swaggercli_liberty_openapi_v2_CLI.hck.json --deltamodel=./swaggercli/rix_swaggercli_liberty_openapi_deltamodel.json;
 
 # Comparing two SWAGGERCLI dereferenced models - THIS DOES NOT FINISH
-/Applications/Hackolade.app/Contents/MacOS/Hackolade compMod --model1=/Users/rvanbruggen/Downloads/src/swaggercli/rix_swaggercli_liberty_openapi_dereferenced_v1_CLI.hck.json --model2=/Users/rvanbruggen/Downloads/src/swaggercli/rix_swaggercli_liberty_openapi_dereferenced_v2_CLI.hck.json --deltamodel=./swaggercli/rix_swaggercli_liberty_openapi_dereferenced_deltamodel.json;
+/Applications/Hackolade.app/Contents/MacOS/Hackolade compMod --model1=./swaggercli/rix_swaggercli_liberty_openapi_dereferenced_v1_CLI.hck.json --model2=./swaggercli/rix_swaggercli_liberty_openapi_dereferenced_v2_CLI.hck.json --deltamodel=./swaggercli/rix_swaggercli_liberty_openapi_dereferenced_deltamodel.json;
 
 
 
@@ -49,22 +49,22 @@ redocly bundle --dereferenced ./v2/master.yaml --output ./redocly/rix_redocly_li
 
 
 # Reverse engineer the combined REDOCLY YAML files into a model
-/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="/Users/rvanbruggen/Downloads/src/redocly/rix_redocly_liberty_openapi_v1.yaml" --model=/Users/rvanbruggen/Downloads/src/redocly/rix_redocly_liberty_openapi_v1_CLI.hck.json;
+/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="./redocly/rix_redocly_liberty_openapi_v1.yaml" --model=./redocly/rix_redocly_liberty_openapi_v1_CLI.hck.json;
 
-/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="/Users/rvanbruggen/Downloads/src/redocly/rix_redocly_liberty_openapi_v2.yaml" --model=/Users/rvanbruggen/Downloads/src/redocly/rix_redocly_liberty_openapi_v2_CLI.hck.json;
+/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="./redocly/rix_redocly_liberty_openapi_v2.yaml" --model=./redocly/rix_redocly_liberty_openapi_v2_CLI.hck.json;
 
 # Reverse engineer the combined DEREFERENCED REDOCLY YAML files into a model
-/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="/Users/rvanbruggen/Downloads/src/redocly/rix_redocly_liberty_openapi_dereferenced_v1.yaml" --model=/Users/rvanbruggen/Downloads/src/redocly/rix_redocly_liberty_openapi_dereferenced_v1_CLI.hck.json;
+/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="./redocly/rix_redocly_liberty_openapi_dereferenced_v1.yaml" --model=./redocly/rix_redocly_liberty_openapi_dereferenced_v1_CLI.hck.json;
 
-/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="/Users/rvanbruggen/Downloads/src/redocly/rix_redocly_liberty_openapi_dereferenced_v2.yaml" --model=/Users/rvanbruggen/Downloads/src/redocly/rix_redocly_liberty_openapi_dereferenced_v2_CLI.hck.json;
+/Applications/Hackolade.app/Contents/MacOS/Hackolade revEng --target=OPENAPI --files="./redocly/rix_redocly_liberty_openapi_dereferenced_v2.yaml" --model=./redocly/rix_redocly_liberty_openapi_dereferenced_v2_CLI.hck.json;
 
 
 
 
 # Comparing two REDOCLY models
-/Applications/Hackolade.app/Contents/MacOS/Hackolade compMod --model1=/Users/rvanbruggen/Downloads/src/redocly/rix_redocly_liberty_openapi_v1_CLI.hck.json --model2=/Users/rvanbruggen/Downloads/src/redocly/rix_redocly_liberty_openapi_v2_CLI.hck.json --deltamodel=./redocly/rix_redocly_liberty_openapi_deltamodel.json;
+/Applications/Hackolade.app/Contents/MacOS/Hackolade compMod --model1=./redocly/rix_redocly_liberty_openapi_v1_CLI.hck.json --model2=./redocly/rix_redocly_liberty_openapi_v2_CLI.hck.json --deltamodel=./redocly/rix_redocly_liberty_openapi_deltamodel.json;
 
 # Comparing two DEREFERENCED REDOCLY models - THIS DOES NOT FINISH!
-/Applications/Hackolade.app/Contents/MacOS/Hackolade compMod --model1=/Users/rvanbruggen/Downloads/src/redocly/rix_redocly_liberty_openapi_dereferenced_v1_CLI.hck.json --model2=/Users/rvanbruggen/Downloads/src/redocly/rix_redocly_liberty_openapi_dereferenced_v2_CLI.hck.json --deltamodel=./redocly/rix_redocly_liberty_openapi_dereferenced_deltamodel.json;
+/Applications/Hackolade.app/Contents/MacOS/Hackolade compMod --model1=./redocly/rix_redocly_liberty_openapi_dereferenced_v1_CLI.hck.json --model2=./redocly/rix_redocly_liberty_openapi_dereferenced_v2_CLI.hck.json --deltamodel=./redocly/rix_redocly_liberty_openapi_dereferenced_deltamodel.json;
 
 
